c3e5c98551f3512899820f3dd5547878d7a02154
Bzu Mah <bzumah9@gmail.com>
2025-11-19 15:19:34 +0545

    wrapper

--- build/PythonNetWrapper/obj/Debug/net6.0/PythonNetWrapper.AssemblyInfo.cs
+++ build/PythonNetWrapper/obj/Debug/net6.0/PythonNetWrapper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PythonNetWrapper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e609fa649e16a81859282a2d9280440d139054c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d72aa1f8b592bd91b68ea4324eccd62380c5a92")]
 [assembly: System.Reflection.AssemblyProductAttribute("PythonNetWrapper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PythonNetWrapper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
